cd4a99d5f7047ce9f66555445ec69ee7cc173ca9
michael.milich <michael.milich1@gmail.com>
2025-01-14 10:19:36 +0200

    is odd checker after code review by chatGPT

--- new_c_excersizes/1_1_is_odd_checker.c
+++ new_c_excersizes/1_1_is_odd_checker.c
@@ -1,45 +1,82 @@
+/*
+Write a program that takes a number as input and uses if-else statements to check whether the number is even or odd.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <limits.h>
 
-#define TRUE 1
-#define FALSE 0
-// Write a program that takes a number as input and uses if-else statements to check whether the number is even or odd.
-
-
-int is_even(int number){
-    if (number %2 ==0){
-        return TRUE;
-    }
-    return FALSE;
+bool is_even(int number) {
+    return number % 2 == 0;
 }
 
-int test_is_even(){
-    // testing if the number is odd
+// Test functions
+bool test_is_even_with_odd_number() {
     int num = 3;
-    int expected = FALSE;
-    
-
+    return !is_even(num); // Expect false
 }
 
-int main(int argc, char *argv[])
-{
-    // Check if an argument is provided
+bool test_is_even_with_even_number() {
+    int num = 2;
+    return is_even(num); // Expect true
+}
+
+bool test_is_even_with_zero() {
+    int num = 0;
+    return is_even(num); // Expect true
+}
+
+bool test_is_even_with_large_number() {
+    int num = INT_MAX;
+    return !is_even(num); // Expect false
+}
+
+bool run_tests() {
+    if (!test_is_even_with_odd_number()) {
+        printf("Test failed: Odd number test\n");
+        return false;
+    }
+    if (!test_is_even_with_even_number()) {
+        printf("Test failed: Even number test\n");
+        return false;
+    }
+    if (!test_is_even_with_zero()) {
+        printf("Test failed: Zero test\n");
+        return false;
+    }
+    if (!test_is_even_with_large_number()) {
+        printf("Test failed: Large number test\n");
+        return false;
+    }
+    return true;
+}
+
+int main(int argc, char *argv[]) {
+    // Run tests
+    if (!run_tests()) {
+        return 1;
+    }
+
+    // Check input arguments
     if (argc != 2) {
         printf("Usage: %s <integer>\n", argv[0]);
         return 1;
     }
 
-    // Convert the argument from string to integer
-    int num = atoi(argv[1]);
-    if(num==0){
-        printf("input number was 0, zero is zero \n");
-        return 0;
+    char *endptr;
+    long num = strtol(argv[1], &endptr, 10);
+
+    if (*endptr != '\0' || num < INT_MIN || num > INT_MAX) {
+        printf("Invalid input. Please enter a valid integer.\n");
+        return 1;
     }
-    
-    if(is_even(num)){
-        printf("%d is an even number! \n", num);
+
+    if (is_even((int)num)) {
+        printf("%ld is an even number.\n", num);
     } else {
-        printf("%d is an odd number! \n ", num);
+        printf("%ld is an odd number.\n", num);
     }
+
     return 0;
 }
