fdd2b5cef56fe48a7b311241f6771e0b55198e75
michael.milich <michael.milich1@gmail.com>
2025-05-17 17:42:37 +0300

    calculator

--- new_c_excersizes/5_2_calculator_cli.c
+++ new_c_excersizes/5_2_calculator_cli.c
@@ -1,24 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 #include "calc_lexer.h"
 #include "calc_parser.h"
 
+void print_help(const char *program_name) {
+    printf("Usage: %s [--help] [--verbose] \"expression\"\n", program_name);
+    printf("Evaluate a mathematical expression.\n\n");
+    printf("Options:\n");
+    printf("  --help       Show this help message\n");
+    printf("  --verbose    Print debug tokens as they're parsed\n");
+}
+
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Usage: %s \"expression\"\n", argv[0]);
+    if (argc < 2 || argc > 3) {
+        print_help(argv[0]);
+        return 1;
+    }
+
+    bool verbose = false;
+    const char *expression = NULL;
+
+    // Handle options
+    for (int i = 1; i < argc; ++i) {
+        if (strcmp(argv[i], "--help") == 0) {
+            print_help(argv[0]);
+            return 0;
+        } else if (strcmp(argv[i], "--verbose") == 0) {
+            verbose = true;
+        } else {
+            expression = argv[i];
+        }
+    }
+
+    if (expression == NULL) {
+        fprintf(stderr, "[ERROR] No expression provided.\n");
+        print_help(argv[0]);
         return 1;
     }
 
     Lexer lexer;
-    lexer_init(&lexer, argv[1]);
+    lexer_init(&lexer, expression);
+
+    // Verbose mode: print tokens
+    if (verbose) {
+        printf("Tokens:\n");
+        Token *t;
+        do {
+            t = lexer_get_next_token(&lexer);
+            if (t->type == TOKEN_NUMBER)
+                printf("TOKEN_NUMBER: %f\n", t->value);
+            else
+                printf("TOKEN: %s\n", token_type_name(t->type));
+        } while (t->type != TOKEN_EOF);
+
+        // Reset lexer before parsing again
+        lexer_init(&lexer, expression);
+    }
+
     float result;
-    bool did_calculate = parse_expression(&lexer,&result);
-    if(!did_calculate){
-        printf("[ERROR] input was in the wrong format. stoping calculation \n");
+    if (!parse_expression(&lexer, &result)) {
+        printf("[ERROR] Invalid expression.\n");
         return 1;
     }
+
     printf("Result: %f\n", result);
     return 0;
 }
--- utils/calc_token.h
+++ utils/calc_token.h
@@ -15,6 +15,23 @@ typedef enum {
     TOKEN_INVALID
 } TokenType;
 
+const char *token_type_name(TokenType type) {
+    switch (type) {
+        case TOKEN_NUMBER:      return "TOKEN_NUMBER";
+        case TOKEN_PLUS:        return "TOKEN_PLUS";
+        case TOKEN_MINUS:       return "TOKEN_MINUS";
+        case TOKEN_STAR:        return "TOKEN_STAR";
+        case TOKEN_SLASH:       return "TOKEN_SLASH";
+        case TOKEN_CARET:       return "TOKEN_CARET";
+        case TOKEN_LPAREN:      return "TOKEN_LPAREN";
+        case TOKEN_RPAREN:      return "TOKEN_RPAREN";
+        case TOKEN_EOF:         return "TOKEN_EOF";
+        case TOKEN_INVALID:     return "TOKEN_INVALID";
+        case TOKEN_UNARY_MINUS: return "TOKEN_UNARY_MINUS";
+        default:                return "UNKNOWN_TOKEN";
+    }
+}
+
 typedef struct {
     TokenType type;
     float value;  // Only used if type == TOKEN_NUMBER
